660d0c4767a682ab52f96080e7a90b08f2332510
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-08-21 10:53:16 -0300

    viewssets

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for bookstore project.
+"""bookstore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
 ]
