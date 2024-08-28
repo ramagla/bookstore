5fe917722381780b9ccb8f5dcd1336fafb1e5291
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-08-28 08:49:31 -0300

    gitanywhere3

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -19,10 +19,14 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
 
+from bookstore import views
+
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
-    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),   
+    path("update_server/", views.update, name="update"),
+  
     # path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
