3d109ae9e2005919026c5bb417e56c989c5a8613
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-08-28 08:59:27 -0300

    gitanywhere4

--- bookstore/views.py
+++ bookstore/views.py
@@ -13,7 +13,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo('/home/ramagla/bookstore')
+        repo = git.Repo('/home/drsantos20/bookstore')
         origin = repo.remotes.origin
 
         origin.pull()
